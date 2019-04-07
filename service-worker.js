821a01deef50a1870a683e11e00d5282f9ab5778
Rodrigo Gomez Luque <rogolu90@gmail.com>
2019-04-07 20:07:21 +0200

    Edit initial text
    Signed-off-by: Rodrigo Gomez Luque <rogolu90@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   console.log('Service worker installing...');
-  // Add a call to skipWaiting here
+
 });
 
 self.addEventListener('activate', event => {
